bb634b9bfe76fbed0daeeeb6b1cb9d5683fe2f77
青崖白鹿 <whited@qingyabailudeMacBook-Air.local>
2026-07-31 15:22:50 +0800

    modify the url of ppts

--- src/data/courses.js
+++ src/data/courses.js
@@ -1,31 +1,54 @@
 const courseData = [
   {
-    id: 'chemistry',
-    title: '化学',
+    id: "chemistry",
+    title: "化学",
     children: [
       {
-        id: 'chemistry-courseware',
-        title: '课件',
+        id: "chemistry-courseware",
+        title: "课件",
         children: [
-          { id: 'chemistry-courseware-1', title: '配位化学科普.pptx', url: '/ppts/配位化学科普.pptx' },
+          {
+            id: "chemistry-courseware-1",
+            title: "配位化学科普.pptx",
+            url: "https://pub-b4b3ad06688f4d63935692118dc7fab1.r2.dev/ppts/配位化学科普.pptx",
+          },
+        ],
+      },
+      {
+        id: "chemistry-experiment",
+        title: "实验",
+        children: [
+          {
+            id: "chemistry-experiment-1",
+            title: "生活中的科学——三个趣味实验.pptx",
+            url: "https://pub-b4b3ad06688f4d63935692118dc7fab1.r2.dev/ppts/生活中的科学——三个趣味实验.pptx",
+          },
         ],
       },
     ],
   },
   {
-    id: 'biology',
-    title: '生物',
+    id: "biology",
+    title: "生物",
     children: [
       {
-        id: 'biology-courseware',
-        title: '课件',
+        id: "biology-courseware",
+        title: "课件",
         children: [
-          { id: 'biology-courseware-1', title: '蛋白质.pptx', url: '/ppts/蛋白质：生命活动的主要承担者.pptx' },
-          { id: 'biology-courseware-2', title: '生命的蓝图和工具.pptx', url: '/ppts/生命的蓝图和工具.pptx' },
+          {
+            id: "biology-courseware-1",
+            title: "蛋白质.pptx",
+            url: "https://pub-b4b3ad06688f4d63935692118dc7fab1.r2.dev/ppts/蛋白质：生命活动的主要承担者.pptx",
+          },
+          {
+            id: "biology-courseware-2",
+            title: "生命的蓝图和工具.pptx",
+            url: "https://pub-b4b3ad06688f4d63935692118dc7fab1.r2.dev/ppts/生命的蓝图和工具.pptx",
+          },
         ],
       },
     ],
   },
-]
+];
 
-export default courseData
+export default courseData;
